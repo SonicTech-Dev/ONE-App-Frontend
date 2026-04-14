a695528510147b46a4f30e80e42bc95dc9a2af1f
SonicTech-Dev <sonictechae@gmail.com>
2026-04-14 11:34:33 +0400

    updated app , pushing to repo building ios version

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -229,6 +229,10 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     marginBottom: 10,
     padding: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
     elevation: 3,
   },
   image: {
